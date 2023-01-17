5ba5e19636742136bdd826c50ff5e40047d69896
jogopin <jonn.g.pineda@gmail.com>
2023-01-17 16:36:55 +0100

    display movie img and recommended if rating>8

--- src/components/Main.js
+++ src/components/Main.js
@@ -12,21 +12,29 @@ export default function Main(){
         const newListOfMovies = moviesArray.filter(movie=>(movie.id!==id))
         setMoviesArray(newListOfMovies)
     }
-
+    const titleMessage = moviesArray.length>0 ? <h2> Current number of movies: {moviesArray.length}</h2> : <h2>there are no movies, Sorry.</h2>
     return(
     <div className="Main">
-    {moviesArray.length>0 ? <h2> Current number of movies: {moviesArray.length}</h2> : <h2>there are no movies, Sorry.</h2>}
+        {titleMessage}
         <div className="movies-list">
 
             {moviesArray.map(movie=>{
                 return (
                     <div className="movie card" key={movie.id}>
-                        <img src= {movie.imgURL} alt="movie.poster"/>
+
+                        {movie.imgURL 
+                        ? <img src= {movie.imgURL} alt="movie.poster"/> 
+                        : <img src= "https://via.placeholder.com/182x268" alt="movie.poster"/> 
+                        }
                         <h2>{movie.title}</h2>
                         {/* <h3>Genre: {movie.genres.map((genre,index)=>(<span key={index}>{genre} &nbsp;</span>))} </h3> */}
                         <h3>Genre2: {movie.genres.join(", ")}</h3>
                         <h3>Year:{movie.year}</h3>
                         <h3>Rating: {movie.rating}</h3>
+                        
+                        {/* conditional rendering with logical && operator */}
+                        {movie.rating >=8 && <h3 className="badge" >Recommended</h3>}
+
                         <button onClick={()=>{ deleteMovie(movie.id) }} >Delete this movie</button>
                     </div>
                 )
